9e01934b2c1eaa44c3bd286adf9b5bba2b46fa30
Alex <a.chisar00@gmail.com>
2021-06-07 22:59:48 -0400

    setup html routes file

---
+++ routes/htmlRoute.js
@@ -0,0 +1,7 @@
+const path = require('path');
+
+app.get('/', (req, res) => res.sendFile(path.join(_dirname, '../public/index.html')));
+
+app.get('/notes', (req, res) => res.sendFile(path.join(_dirname, '../public/notes.html')))
+
+module.export = app;
--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+const routes = require('./routes');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
+app.use(routes);
 
 app.listen(PORT, () => console.log(`App listening on PORT: ${PORT}`));
